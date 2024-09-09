7fc5222d0940fd5c5e04f36c7227f5d8154b8b3a
JuanCamiloRodriguezT <juancam@tabiconnect.com>
2024-09-09 10:16:55 -0500

    e

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from app.main import app  # Importa tu aplicación FastAPI
 import json
 
-with open("./openapi.json", "w") as f:
+with open("./api-rtr/openapi.json", "w") as f:
     json.dump(app.openapi(), f)
